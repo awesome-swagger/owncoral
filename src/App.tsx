052e38c196ca625807a49b5d743ba1d171105c51
awesome-swagger <aquiljohnston3@gmail.com>
2021-03-10 22:26:04 +0500

    code spliting on routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, Spinner, Flex } from '@chakra-ui/react';
 import { Global } from '@emotion/react';
 
 import AppRootStyle from './AppRootStyle';
@@ -12,12 +12,13 @@ import { UserContext } from './userContext';
 
 import { DebugPanel } from './components';
 
-import Signup from './pages/signup';
-import Portfolio from './pages/portfolio';
 import { default as Login, ForgotCheckEmail, ForgotPassword, NewPassword } from './pages/login';
-import LoginFlow from './pages/investment-profile/steps';
-import Property from './pages/property';
-import Profile from './pages/profile';
+
+const Portfolio = lazy(() => import('./pages/portfolio'));
+const Profile = lazy(() => import('./pages/profile'));
+const Property = lazy(() => import('./pages/property'));
+const LoginFlow = lazy(() => import('./pages/investment-profile/steps'));
+const Signup = lazy(() => import('./pages/signup'));
 
 const headerStyles = { h1, h2, h3, h4, h5, h6 };
 
@@ -29,26 +30,28 @@ function App() {
       <ChakraProvider theme={AppTheme}>
         <Global styles={[AppRootStyle, headerStyles]} />
         <DebugPanel />
-        <Router>
-          {/* Note: server handles not-logged-in redirection for the SPA bundle */}
-          {/*
+        <Suspense fallback={<Loading />}>
+          <Router>
+            {/* Note: server handles not-logged-in redirection for the SPA bundle */}
+            {/*
             TODO: refactor to just use nested routes / URL parameters
             https://reactrouter.com/web/guides/philosophy/nested-routes
           */}
-          <Signup />
-          <LoginFlow /> {/* setUser={setUser} */}
-          {/* TODO: Redirect not-signed-up users to signup */}
-          <Route exact path="/">
-            <Redirect to="/portfolio" />
-          </Route>
-          <AuthRoutes />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/property/:address" component={Property} />
-          <Route exact path="/profile" component={Profile} />
-          {/* <Route exact path="/new-opportunities" component={Opportunity} /> */}
-          {/* <Route exact path="/documents" component={Docs} /> */}
-          {/* <Route exact path="/new-opportunities/:id" component={OpportunityDetail} /> */}
-        </Router>
+            <Signup />
+            <LoginFlow /> {/* setUser={setUser} */}
+            {/* TODO: Redirect not-signed-up users to signup */}
+            <Route exact path="/">
+              <Redirect to="/portfolio" />
+            </Route>
+            <AuthRoutes />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/property/:address" component={Property} />
+            <Route exact path="/profile" component={Profile} />
+            {/* <Route exact path="/new-opportunities" component={Opportunity} /> */}
+            {/* <Route exact path="/documents" component={Docs} /> */}
+            {/* <Route exact path="/new-opportunities/:id" component={OpportunityDetail} /> */}
+          </Router>
+        </Suspense>
       </ChakraProvider>
     </UserContext.Provider>
   );
@@ -81,6 +84,12 @@ async function getUser(setUser: (u: UserT) => void): Promise<void> {
     setUser(await resp.json());
   }
 }
+
+const Loading = () => (
+  <Flex w="100vw" h="100vh" justifyContent="center" alignItems="center">
+    <Spinner size="xl" thickness="4px" speed="0.65s" emptyColor="gray.200" color="blue.500" />
+  </Flex>
+);
 
 // eslint-disable-next-line import/no-default-export
 export default App;
