240f318d58f2da560b1f112f49bffdbb198a7ce8
awesome-swagger <aquiljohnston3@gmail.com>
2021-05-18 13:25:05 +0500

    custom slider arrow

--- src/components/imgSlider/index.tsx
+++ src/components/imgSlider/index.tsx
@@ -1,11 +1,81 @@
 import { Carousel } from 'react-responsive-carousel';
-import { Image } from '@chakra-ui/react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './style.css';
+import { Image, Icon } from '@chakra-ui/react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 
 export const ImgSlider = ({ images }: { images: any }) => {
+  const arrowStyles: React.CSSProperties = {
+    position: 'absolute',
+    zIndex: 2,
+    top: 'calc(50% - 15px)',
+    cursor: 'pointer',
+    outline: 'none',
+    background: '#00000050',
+    borderRadius: '50%',
+  };
+
+  const indicatorStyles: React.CSSProperties = {
+    background: '#fff',
+    width: 10,
+    height: 10,
+    display: 'inline-block',
+    margin: '0 8px',
+    borderRadius: '50%',
+    outline: 'none',
+    opacity: '.7',
+  };
+
   return (
-    <Carousel showThumbs={false} infiniteLoop={true}>
+    <Carousel
+      showStatus={false}
+      renderArrowPrev={(onClickHandler, hasPrev, label) =>
+        hasPrev && (
+          <button
+            type="button"
+            onClick={onClickHandler}
+            title={label}
+            style={{ ...arrowStyles, left: 5 }}
+          >
+            <Icon as={FiChevronLeft} color="#FFF" h={8} w={8} />
+          </button>
+        )
+      }
+      renderArrowNext={(onClickHandler, hasNext, label) =>
+        hasNext && (
+          <button
+            type="button"
+            onClick={onClickHandler}
+            title={label}
+            style={{ ...arrowStyles, right: 5 }}
+          >
+            <Icon as={FiChevronRight} color="#FFF" h={8} w={8} />
+          </button>
+        )
+      }
+      renderIndicator={(onClickHandler, isSelected, index, label) => {
+        if (isSelected) {
+          return (
+            <li
+              style={{ ...indicatorStyles, background: '#000' }}
+              aria-label={`Selected: ${label} ${index + 1}`}
+              title={`Selected: ${label} ${index + 1}`}
+            />
+          );
+        }
+        return (
+          <li
+            style={indicatorStyles}
+            onClick={onClickHandler}
+            onKeyDown={onClickHandler}
+            value={index}
+            key={index}
+            role="button"
+            tabIndex={0}
+            title={`${label} ${index + 1}`}
+            aria-label={`${label} ${index + 1}`}
+          />
+        );
+      }}
+    >
       {images.map((value: string) => (
         <div>
           <Image src={value} alt={value} />
