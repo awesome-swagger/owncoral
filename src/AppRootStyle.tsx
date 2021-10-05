297392f8112644253f22ed0abf801800b51f33f1
awesome-swagger <aquiljohnston3@gmail.com>
2021-10-05 19:27:54 +0500

    responsive font size

--- src/AppRootStyle.tsx
+++ src/AppRootStyle.tsx
@@ -15,11 +15,11 @@ const AppRootStyle = css`
 
   /*
   TODO: re-enable if we need to flex on font size
+  */
   html {
-    // We size text at 85% at 360px, and 100% at 768px
-    font-size: calc(85% + clamp(0%, 1.6 * ((100vw - 360px) / 408), 15%));
+    // We size text at 100% at 360px, and 110% at 768px
+    font-size: calc(100% + clamp(0%, 1.6 * ((100vw - 360px) / 408), 10%));
   }
-   */
 `;
 
 // eslint-disable-next-line import/no-default-export
